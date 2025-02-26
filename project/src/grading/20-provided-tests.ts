8ef6032a24978cb1381104f829e21f290b88b68a
Yashwant Bitra <65294199+yashzord@users.noreply.github.com>
2025-02-26 02:36:28 -0500

    Update 20-provided-tests.ts

--- project/src/grading/20-provided-tests.ts
+++ project/src/grading/20-provided-tests.ts
@@ -1,15 +1,15 @@
-import { GradeTypes, makeMochaSuite } from 'gradescope-lib';
+import { GradeTypes, makeCmdTest } from 'gradescope-lib';
 
-const PRJ_DIR = process.env['PRJ_DIR'] ?? '.';
+const CMD = `cd ${process.env.PRJ_DIR} && node dist/hw1-tests.js`;
 
 const SUITE_OPTS: GradeTypes.TestSuiteOpts = {
   abortOnFail: false,
   visibility: 'visible',
-  name: 'Provided Mocha Tests',
+  name: 'Provided Tests',
+  timeoutMillis: 60_000,
 };
 
-const TEST_PATH = 'dist/grading-tests/hw1-tests.js'; // This file contains all test cases
-
-const MOCHA_TEST_SUITE = makeMochaSuite(PRJ_DIR, TEST_PATH, SUITE_OPTS);
-
-export default MOCHA_TEST_SUITE;
+export default new GradeTypes.TestSuite(
+  [ makeCmdTest(CMD, { name: 'Run Student Tests' }) ],
+  SUITE_OPTS
+);
